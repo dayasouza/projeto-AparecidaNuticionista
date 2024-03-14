e8d767f43217698d1dfce2d6b02c074c4ac0fb3e
Daya Souza <dayanalvesouza@gmail.com>
2024-03-14 17:28:04 -0300

    Criado variáveis e calculado o imc do primeiro paciente

--- js/princial.js
+++ js/princial.js
@@ -1,2 +1,19 @@
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
+
+var paciente = document.querySelector('#primeiro-paciente');
+
+var tdpeso = paciente.querySelector('.info-peso');
+var peso = tdpeso.textContent;
+
+var tdaltura = paciente.querySelector('.info-altura');
+var altura = tdaltura.textContent;
+
+//Calcular o imc
+
+var imc = peso / (altura * altura);
+
+console.log(paciente);
+console.log(tdaltura);
+console.log(altura);
+console.log(imc);
