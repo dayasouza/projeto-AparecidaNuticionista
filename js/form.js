bbc2f2d0027d471151b6897da6f8e6a0297f4e62
Dayana Souza <dayanalvesouza@gmail.com>
2024-03-19 15:16:11 -0300

    Criado as validações do formulário

--- js/form.js
+++ js/form.js
@@ -2,20 +2,35 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (event) {
   event.preventDefault();
   var form = document.querySelector("#form-adiciona");
-  var paciente = obtemPacienteDoFormulario(form);
+  var paciente = obtemPacienteDoFormulario(form); 
 
-  if (!validaPaciente(paciente)) {
-    console.log("paciente inválido!");
+  var erros = validaPaciente(paciente);
+
+  if (erros.length > 0) {
+    exibeMensagensDeErros(erros);
     return;
   }
 
   var pacienteTr = montarTr(paciente);
 
   var tabela = document.querySelector("#tabela-pacientes");
-
   tabela.appendChild(pacienteTr);
   form.reset();
+
+  var mensagensErro = document.querySelector("#mensagens-erro");
+  mensagensErro.innerHTML = "";
 });
+
+function exibeMensagensDeErros(erros) {
+  var ul = document.querySelector("#mensagens-erro");
+  ul.innerHTML = "";
+
+  erros.forEach(function (erro) {
+    var li = document.createElement("li");
+    li.textContent = erro;
+    ul.appendChild(li);
+  });
+}
 
 function obtemPacienteDoFormulario(form) {
   var paciente = {
@@ -51,9 +66,24 @@ function montaTd(dado, classe) {
 }
 
 function validaPaciente(paciente) {
-if (validaPeso(paciente.peso) || validaAltura(paciente.altura)) {
-    return true;
-  } else {
-    return false;
+  var erros = [];
+
+  if (paciente.nome.length == 0) {
+    erros.push("O nome não pode ser em branco");
   }
+  if (paciente.peso.length == 0) {
+    erros.push("O peso não pode ser em branco");
+  }
+  if (paciente.altura.length == 0) {
+    erros.push("A altura não pode ser em branco");
+  }
+  if (paciente.gordura.length == 0) {
+    erros.push("A gordura não pode ser em branco");
+  }
+  if (!validaPeso(paciente.peso)) erros.push("Peso é inválido"); //Forma diferente de fazer if
+  if (!validaAltura(paciente.altura)) {
+    erros.push("A altura é inválida");
+  }
+
+  return erros;
 }
