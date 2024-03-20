e07d0bd02c8102ece7a835cd5d3d3c5f30d305d8
Dayana Souza <dayanalvesouza@gmail.com>
2024-03-20 15:55:41 -0300

    Utilizado regex para filtrar a busca de pacientes

--- js/filtra.js
+++ js/filtra.js
@@ -7,16 +7,18 @@ campoFiltro.addEventListener("input", function () {
       var paciente = pacientes[i];
       var tdNome = paciente.querySelector(".info-nome");
       var nome = tdNome.textContent;
+      var expressao = new RegExp(this.value, "i");
 
-      if (nome != this.value) {
-        paciente.classList.add(".invisivel");
+      if (!expressao.test(nome)) {
+        paciente.classList.add("invisivel");
       } else {
-        paciente.classList.remove(".invisivel");
+        paciente.classList.remove("invisivel");
       }
     }
   } else {
     for (var i = 0; i < pacientes.length; i++) {
-      paciente.classList.remove(".invisivel");
+      var paciente = pacientes[i];
+      paciente.classList.remove("invisivel");
     }
   }
 });
